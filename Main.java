a0ea7cbf482529805d9e50152b7fc46ff154458d
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-09-15 16:54:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+
+public class Main
+{
+  public static void main(String []args)
+ {
+    Diji s=new Diji();
+    s.calculate();
+ }
+
+
+
+}
+
