6316df7287af806c72de4d7076b80df065157448
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-10-19 21:03:51 +0530

    Create TcpServer.java

---
+++ TcpServer.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+
+public class TcpServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket sersock = new ServerSocket(4000);
+        System.out.println("Server Connected, waiting for client");
+        Socket sock = sersock.accept();
+        System.out.println("Connection successful, waiting for filename");
+        InputStream iStream = sock.getInputStream();
+        BufferedReader nameRead = new BufferedReader(new InputStreamReader(iStream));
+        String fname = nameRead.readLine();
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+        try {
+            BufferedReader contentRead = new BufferedReader(new FileReader(fname));
+            String str;
+            while ((str = contentRead.readLine()) != null) {
+                pwrite.println(str);
+            }
+            contentRead.close();
+        } catch (FileNotFoundException e) {
+            pwrite.println("File does not exist");
+        } finally {
+            System.out.println("Closing connection");
+            pwrite.close();
+            nameRead.close();
+            sock.close();
+            sersock.close();
+        }
+    }
+ }
