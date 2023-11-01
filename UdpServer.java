2005b51a5d56741703dabe04bbf0a93dc7c023e4
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-01 20:45:37 +0530

    Create UdpServer.java

---
+++ UdpServer.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+class UdpServer
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner in = new Scanner(System.in);
+		DatagramSocket datagramSocket = new DatagramSocket();
+		InetAddress clientAddress = InetAddress.getByName("127.0.0.1");
+		String message;
+		byte[] buffer;
+		DatagramPacket datagramPacket;
+		System.out.println("Enter messages to send: ");
+		while (true){
+			message = in.nextLine();
+			buffer = message.getBytes();
+			datagramPacket = new DatagramPacket(buffer, buffer.length, clientAddress, 4000);
+			datagramSocket.send(datagramPacket);
+
+			if (message.equalsIgnoreCase("exit")) {
+				datagramSocket.close();
+				break;
+			}
+
+		}
+	}
+
+}
