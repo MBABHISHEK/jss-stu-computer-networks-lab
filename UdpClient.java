d905e3e1b341d6f0d4fe3da8776343e9cf8d647a
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-01 20:44:06 +0530

    Create UdpClient.java

---
+++ UdpClient.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+
+class UdpClient
+{
+	public static void main(String args[]) throws Exception
+	{
+		DatagramSocket datagramSocket = new DatagramSocket(4000);
+		byte[] buffer;
+		DatagramPacket datagramPacket;
+		System.out.println("Received Messages: ");
+		while(true)
+		{
+			buffer = new byte[65555];
+			datagramPacket = new DatagramPacket(buffer, buffer.length);
+			datagramSocket.receive(datagramPacket);
+			String received = new String(buffer).trim();
+			System.out.println(received);
+			if (received.equalsIgnoreCase("exit")) {
+				datagramSocket.close();
+				break;
+			}
+		}
+	}
+}
