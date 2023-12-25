86771e9d90ac19a3740a44418c3e69d787e42472
ABHI <mba384420@gmail.com>
2023-12-25 13:57:22 +0530

    new

--- Part A/LeakyBucket.java
+++ Part A/LeakyBucket.java
@@ -2,30 +2,35 @@ import java.util.Scanner;
 
 public class LeakyBucket {
     public static void main(String[] args) {
-        int noOfQueries = 4;
-        int bucketSize = 10;
+        int noOfQueries;
+        int bucketSize;
         int inputPacketSize;
-        int outputPacketSize = 1;
+        int outputPacketSize;
         int storedBufferSize = 0;
         int sizeLeft;
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("ENTER THE BUCKET SIZE");
+        bucketSize = scanner.nextInt();
+        System.out.println("ENTER THE NUMBER OF PACKETS");
+        noOfQueries = scanner.nextInt();
+        System.out.println("ENTER THE OUTPUT RATE");
+        outputPacketSize = scanner.nextInt();
 
         for (int i = 0; i < noOfQueries; i++) {
-            System.out.print("ENTER THE NUMBER OF PACKETS ");
+            System.out.print("ENTER THE SIZE OF THE PACKET :");
             inputPacketSize = scanner.nextInt();
 
             sizeLeft = bucketSize - storedBufferSize;
 
             if (inputPacketSize <= sizeLeft) {
                 storedBufferSize += inputPacketSize;
+                System.out.println("Number of Packets added into Bucket" + inputPacketSize);
+            } else {
+                System.out.println("Number of Packets Dropped " + (storedBufferSize + inputPacketSize - bucketSize));
+                storedBufferSize = bucketSize;
             }
-             else {
-                System.out.println("Number of Packets Dropped " + (storedBufferSize+inputPacketSize-bucketSize));
-                storedBufferSize=bucketSize;
-            }
-
-            System.out.println("Stored Buffer Size: " + storedBufferSize);
+            /* System.out.println("Stored Buffer Size: " + storedBufferSize); */
             storedBufferSize -= outputPacketSize;
         }
 
